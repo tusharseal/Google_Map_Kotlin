95624c0e938c51af574fba47c490abda3536e13a
Tushar Seal <tusharseal1@gmail.com>
2024-01-18 12:26:03 +0530

    Google Map Kotlin

---
+++ app/src/main/java/com/example/googlemapkotlin/MapsActivity.kt
@@ -0,0 +1,51 @@
+package com.example.googlemapkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.googlemapkotlin.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        mMap = googleMap
+        val mudLatLng = LatLng(28.762139, 77.505818)
+        val markerOptions = MarkerOptions().position(mudLatLng).title("Muradagar")
+        mMap.addMarker(markerOptions)
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mudLatLng))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mudLatLng, 16f))
+
+
+    }
+}
